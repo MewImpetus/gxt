c21731cfa58ca534ab95a4c46dae43ee41e0d00f
geb <853934146@qq.com>
2024-12-12 22:48:43 +0800

    feat: add GXT Jetton contract

--- tests/GXT.spec.ts
+++ tests/GXT.spec.ts
@@ -1,17 +1,39 @@
-import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
-import { toNano } from '@ton/core';
+import { Blockchain, printTransactionFees, SandboxContract, TreasuryContract } from '@ton/sandbox';
+import { comment, toNano } from '@ton/core';
 import { GXT } from '../wrappers/GXT';
 import '@ton/test-utils';
+
+import { loadTakeWalletAddress, loadTep64TokenData } from "../build/GXT/tact_GXT";
+import { JettonWalletTemplate } from "../build/GXT/tact_JettonWalletTemplate";
+import exp from 'constants';
+import { JettonMasterGXT } from '../build/GXT/tact_JettonMasterGXT';
 
 describe('GXT', () => {
     let blockchain: Blockchain;
     let deployer: SandboxContract<TreasuryContract>;
     let gXT: SandboxContract<GXT>;
 
+    let admin: SandboxContract<TreasuryContract>;
+    let user: SandboxContract<TreasuryContract>;
+    let responseDestination: SandboxContract<TreasuryContract>;
+    let jettonMasterContract: SandboxContract<JettonMasterGXT>;
+    let adminJettonWallet: SandboxContract<JettonWalletTemplate>;
+    let userJettonWallet: SandboxContract<JettonWalletTemplate>;
+    let nJettonOwnerHas: bigint = toNano(Math.random() * 1000);
+
     beforeEach(async () => {
         blockchain = await Blockchain.create();
+        admin = await blockchain.treasury('admin');
+        user = await blockchain.treasury('user');
+        responseDestination = await blockchain.treasury('responseDestination');
 
-        gXT = blockchain.openContract(await GXT.fromInit());
+        gXT = blockchain.openContract(await GXT.fromInit( admin.address,
+            {
+                $$type: "Tep64TokenData",
+                flag: BigInt(1),
+                content: "https://s3.laisky.com/uploads/2024/09/jetton-sample.json",
+            }, 
+            toNano("210000000000")));
 
         deployer = await blockchain.treasury('deployer');
 
